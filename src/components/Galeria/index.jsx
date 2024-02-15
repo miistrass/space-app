d9a3872c9c6487d4e505abe8f618f298edf809c9
Milena Strassburger <milenastrass@gmail.com>
2024-02-15 17:03:54 -0300

    adicionando a galeria de imagens

--- src/components/Galeria/index.jsx
+++ src/components/Galeria/index.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components"
 import { Titulo } from "../Titulo"
 import { Populares } from "./Populares"
 import { Tags } from "./Tags"
+import { Imagem } from "./Imagem"
 
 const GaleriaContainer = styled.div`
     display: flex;
@@ -26,7 +27,9 @@ export const Galeria = ({ fotos = [] }) => {
                 <SecaoFluida>
                     <Titulo>Navegue pela galeria</Titulo>
                     <ImagensContainer>
-                        
+                        {fotos.map(foto => <Imagem
+                            key={foto.id}
+                            foto={foto} />)}
                     </ImagensContainer>
                 </SecaoFluida>
                 <Populares />
